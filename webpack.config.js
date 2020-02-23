b67a7fd7ed5c79c9bdfe5017ff3ec547185d8ca1
Ana <anacarolina.gerhardt@hotmail.com>
2020-02-23 03:34:12 -0300

    Set up initial files

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.js',
+  output: { filename: "bundle.js" },
+  module: {
+    rules: [
+        { test: /\.jsx?$/, loader: ['babel-loader', 'eslint-loader'] },
+        {
+            test: /\.css$/,
+            use: [{
+            loader: 'style-loader',
+            }, {
+            loader: 'css-loader',
+            }]
+        },
+    ]
+  },
+  devtool: "cheap-module-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html'
+  })],
+};
