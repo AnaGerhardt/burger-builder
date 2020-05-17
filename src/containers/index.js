6a3c5d8c465e8d17ae4bbe1bcabb0581435873a5
AnaGerhardt <anacarolina.gerhardt@hotmail.com>
2020-05-17 05:10:47 -0300

    Merge

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,8 +1,4 @@
 export * from './Layout/Layout'
 export * from './ErrorHandler/ErrorHandler'
-<<<<<<< HEAD
-export * from './Checkout/Checkout'
-=======
 export * from './Checkout/Checkout'
 export * from './Checkout/ContactData/ContactData'
->>>>>>> 5e8052e... Add Contact Form in Checkout and Orders button in the Navigation bar
